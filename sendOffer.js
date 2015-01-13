a94338b30227a1f7e09b9811ab93cedd6597c7a1
Thomas Schaffner <schaffti1@gmail.com>
2015-01-13 04:20:04 -0500

    adds verification to ensure all required fields exist before attempting to access them

--- sendOffer.js
+++ sendOffer.js
@@ -15,29 +15,45 @@ var exports = module.exports = {}; // Used to export the sendMail function
 	Formats and send an email based on information in body
 */ 
 exports.sendMail = function (body, transporter, callback) {
-	
-	// Set basic mail options
-	var mailOptions = {
-		// REMOVE JIM'S EMAIL
-		from: 'Deanslist', // Should use process.env
-		to: body.recipient,
-		subject: 'Dean\'s list offer',
-	};
+	var fields = ['sender', 'recipient', 'message', 'postPath', 'offer', 'textbook'];
+	var errorMessage = '';
+	var key = '';
 
-	var message = '';
-	message += '<p>Hello!</p>';
-	message += '<p>' + body.sender + ' has';
-	message += ' made an offer for ' + body.textbook + ': ' + body.offer + '</p>';
-	message += '<p>They say:</p>';
-	message += '<p>' + body.message + '</p>';
-	message += '<hr />'
-	message += '<p>To follow up, send an email to ' + body.sender + '.</p>';
-	message += '<p>To view your post, go here: ' + body.postPath + '</p>';
+	// Ensure all fields exist
+	for (var i = 0; i < fields.length; i++) {
+		key = fields[i];
+		if (!(key in body)) {
+			errorMessage += key + ' missing!;';
+		}
+	}
 	
-	mailOptions.html = message;
+	if (errorMessage == '') {	
+		// Set basic mail options
+		var mailOptions = {
+			// REMOVE JIM'S EMAIL
+			from: 'Deanslist', // Should use process.env
+			to: body.recipient,
+			subject: 'Dean\'s list offer',
+		};
 
-	transporter.sendMail(mailOptions, function(error, response) {
-		// console.log(error);
-		callback(error);
-	});
+		var message = '';
+		message += '<p>Hello!</p>';
+		message += '<p>' + body.sender + ' has';
+		message += ' made an offer for ' + body.textbook + ': ' + body.offer + '</p>';
+		message += '<p>They say:</p>';
+		message += '<p>' + body.message + '</p>';
+		message += '<hr />'
+		message += '<p>To follow up, send an email to ' + body.sender + '.</p>';
+		message += '<p>To view your post, go here: ' + body.postPath + '</p>';
+		
+		mailOptions.html = message;
+
+		transporter.sendMail(mailOptions, function(error, response) {
+			// console.log(error);
+			callback(error);
+		});
+	} else {
+		console.log(errorMessage);
+		callback(errorMessage);
+	}
 };
